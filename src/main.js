f54357041403f16bf280be0293e5619a060e6927
yruano <ckrmidjskk@naver.com>
2021-09-04 12:45:43 +0900

    aa

--- src/main.js
+++ src/main.js
@@ -1,130 +1,139 @@
 // 플레이어
 class Player {
-    constructor() {
-        this.x = center.x;
-        this.y = center.y;
-        this.radius = 10;
-        this.moveSpeed = 5;
-        this.color = '#000000';
-        //      플레이어 움직임
-        this.keyUp = false;
-        this.keyDown = false;
-        this.keyRight = false;
-        this.keyLeft = false;
+  constructor() {
+    this.x = center.x;
+    this.y = center.y;
+    this.radius = 10;
+    this.moveSpeed = 5;
+    this.color = '#000000';
+    //      플레이어 움직임
+    this.keyUp = false;
+    this.keyDown = false;
+    this.keyRight = false;
+    this.keyLeft = false;
 
-        window.addEventListener('keydown', event => {
-            event.key == 'w' && (player.keyUp = true);
-            event.key == 's' && (player.keyDown = true);
-            event.key == 'd' && (player.keyRight = true);
-            event.key == 'a' && (player.keyLeft = true);
-        });
-        window.addEventListener('keyup', event => {
-            event.key == 'w' && (player.keyUp = false);
-            event.key == 's' && (player.keyDown = false);
-            event.key == 'd' && (player.keyRight = false);
-            event.key == 'a' && (player.keyLeft = false);
-        });
+    window.addEventListener('keydown', event => {
+      event.key == 'w' && (player.keyUp = true);
+      event.key == 's' && (player.keyDown = true);
+      event.key == 'd' && (player.keyRight = true);
+      event.key == 'a' && (player.keyLeft = true);
+    });
+    window.addEventListener('keyup', event => {
+      event.key == 'w' && (player.keyUp = false);
+      event.key == 's' && (player.keyDown = false);
+      event.key == 'd' && (player.keyRight = false);
+      event.key == 'a' && (player.keyLeft = false);
+    });
+  }
+  // 플레이어를 그리기위해 draw를 가져옴
+  draw() {
+    let { x, y } = wall(this.x, this.y, this.radius);
+    this.x = x;
+    this.y = y;
+    drawCircle(this.x, this.y, this.radius, this.color);
+  }
+  processInput() {
+    this.keyUp && (player.y -= player.moveSpeed);
+    this.keyDown && (player.y += player.moveSpeed);
+    this.keyRight && (player.x += player.moveSpeed);
+    this.keyLeft && (player.x -= player.moveSpeed);
+  }
+  processCollision(enemy) {
+    const dist = distance(player.x, player.y, enemy.x, enemy.y);
+    if (dist <= player.radius + enemy.radius) {
+      gameOver();
+      console.log("hit");
     }
-    // 플레이어를 그리기위해 draw를 가져옴
-    draw() {
-        let { x, y } = wall(this.x, this.y, this.radius);
-        this.x = x;
-        this.y = y;
-        drawCircle(this.x, this.y, this.radius, this.color);
-    }
-    processInput() {
-        this.keyUp && (player.y -= player.moveSpeed);
-        this.keyDown && (player.y += player.moveSpeed);
-        this.keyRight && (player.x += player.moveSpeed);
-        this.keyLeft && (player.x -= player.moveSpeed);
-    }
-    processCollision(enemy) {
-        const dist = distance(player.x, player.y, enemy.x, enemy.y);
-        if (dist <= player.radius + enemy.radius) {
-            gameOver();
-            console.log("hit");
-        }
-    }
+  }
 }
 
 // 적
 class Enemy {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.radius = 10;
-        this.moveSpeed = 3;
-        this.color = '#ff0000';
-    }
-    // 적을 그리기위해 draw를 가져옴
-    draw() {
-        drawCircle(this.x, this.y, this.radius, this.color);
-        wall(this.x, this.y, this.radius);
-    }
-    follow(target) {
-        const dir = direction(this.x, this.y, target.x, target.y);
-        const dist = distanceSq(this.x, this.y, target.x, target.y);
-        const speed = dist < this.moveSpeed ** 2 ? Math.sqrt(dist) : this.moveSpeed;
-        this.x += dir.x * speed;
-        this.y += dir.y * speed;
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 10;
+    this.moveSpeed = 3;
+    this.color = '#ff0000';
+  }
+  // 적을 그리기위해 draw를 가져옴
+  draw() {
+    drawCircle(this.x, this.y, this.radius, this.color);
+    //wall(this.x, this.y, this.radius);
+  }
+  follow(target) {
+    const dir = direction(this.x, this.y, target.x, target.y);
+    const dist = distanceSq(this.x, this.y, target.x, target.y);
+    const speed = dist < this.moveSpeed ** 2 ? Math.sqrt(dist) : this.moveSpeed;
+    this.x += dir.x * speed;
+    this.y += dir.y * speed;
+  }
 }
 
 class Timer {
-    constructor(interval, fn, countr) {
-        this.interval = interval;
-        this.fn = fn;
-        this.countr = countr
-        this.timer = 0;
-        this.val = 5;
+  constructor(interval, fn, countr) {
+    this.interval = interval;
+    this.fn = fn;
+    this.countr = countr
+    this.timer = 0;
+    this.val = 2;
+  }
+  spawntime() {
+    this.timer += deltatime;
+    if (this.timer >= this.interval) {
+      this.fn();
+      this.timer = 0;
+      this.countr += 0.2;
     }
-    spawntime() {
-        this.timer += deltatime;
-        if (this.timer >= this.interval) {
-            this.fn();
-            this.timer = 0;
-            this.countr += 1;
+    if (this.countr >= this.val) {
+      if (this.val <= 0.5) {
+        if (this.interval <= 0.1) {
+          return;
         }
-        if (this.countr == val) {
-            this.interval -= 1;
-            val -= 1;
-            this.countr = 0;
-        }
+        this.interval -= 0.01;
+        console.log(this.interval);
+        return;
+      }
+      this.interval -= 0.8;
+      this.val -= 0.4;
+      this.countr = 0;
+      console.log(this.interval);
     }
+  }
 }
 
 function spawnEnemy() {
-    let enemy = new Enemy(0, 0);
-    let spawnPos = randomspawn(enemy.radius);
-    enemy.x = spawnPos.x;
-    enemy.y = spawnPos.y;
-    return enemy;
+  let enemy = new Enemy(0, 0);
+  let spawnPos = randomspawn(enemy.radius);
+  enemy.x = spawnPos.x;
+  enemy.y = spawnPos.y;
+  return enemy;
 }
 
 const player = new Player();
 const enemies = [];
 
-const spawntime = new Timer(5, () => { enemies.push(spawnEnemy()); console.log('들어와'); })
+const spawntime = new Timer(3, () => { enemies.push(spawnEnemy()); console.log('들어와'); }, 0)
 
 let deltatime = 0;
 let calctime = 0;
 let savetime = 0;
 
 (function loop() {
-    deltatime = (performance.now() - calctime) / 1000;
-    gl.clearRect(0, 0, canvas.width, canvas.height);
+  deltatime = (performance.now() - calctime) / 1000;
+  gl.clearRect(0, 0, canvas.width, canvas.height);
 
-    player.processInput();
-    player.draw();
+  player.processInput();
+  player.draw();
 
-    spawntime.spawntime();
+  spawntime.spawntime();
 
-    for (const enemy of enemies) {
-        player.processCollision(enemy);
-        enemy.follow(player);
-        enemy.draw();
-    }
+  for (const enemy of enemies) {
+    player.processCollision(enemy);
+    enemy.follow(player);
+    enemy.draw();
+  }
 
-    calctime = performance.now();
-    requestAnimationFrame(loop);
+  calctime = performance.now();
+  requestAnimationFrame(loop);
 })();
